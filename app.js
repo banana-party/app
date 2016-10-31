5f4c60fce6567ea556dea48957faf575425207f2
acc-cloudaccess <acc.cloudaccess@gmail.com>
2016-10-31 17:15:46 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ var http = require('http'); //add the http module
 
 //Create a server
 var myServer = http.createServer(function (request, response) {
-  // Return something from server
+
+// Return something from server
   response.writeHead(200, {"Content-Type": "text/plain"});
   response.end("Hello Node\n");
 }); //create a server
 
-Bind server to a port
-myServer.listen(80);
+//Bind server to a port
+myServer.listen(3000);
 
-console.log("Go to localhost:80 on your browser");
+console.log("Go to localhost:3000 on your browser");
